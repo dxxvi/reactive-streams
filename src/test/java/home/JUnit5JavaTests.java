d369cc784e100014d238c1af838e5bc1789326e3
Thai <dxxvi@yahoo.com>
2018-12-26 08:28:41 -0500

    ...

--- src/test/java/home/JUnit5JavaTests.java
+++ src/test/java/home/JUnit5JavaTests.java
@@ -1,10 +1,19 @@
 package home;
 
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JUnit5JavaTests {
     @Test
-    public void test() {
-        System.out.println("Hello JUnit 5 in Java");
+    public void rxjava2() {
+        List<Integer> list = new LinkedList<>();
+        Flowable.range(1, 64)
+                .observeOn(Schedulers.computation())
+                .flatMap(i -> )
     }
 }
--- src/test/scala/home/JUnit5ScalaTests.scala
+++ src/test/scala/home/JUnit5ScalaTests.scala
@@ -4,7 +4,33 @@ import org.junit.jupiter.api.Test
 
 class JUnit5ScalaTests {
     @Test
-    def test() {
-        println("Hello JUnit 5 in Scala")
+    def palindromeSubstrings() {
+        def isPalindrome(s: String): Boolean = s.reverse == s
+
+        def findAllPalindromesUsingBruteForceApproach(s: String): Set[String] = for {
+                i <- (0 until s.length).toSet[Int]
+                j <- (i+1) to s.length
+                substring = s.substring(i, j)
+                if isPalindrome(substring)
+            } yield substring
+
+        println(findAllPalindromesUsingBruteForceApproach("abcdefedefe"))
+
+        @annotation.tailrec
+        def findPalindromes(s: String, low: Int, high: Int, set: Set[String]): Set[String] =
+            if (low < 0 || high >= s.length || s(low) != s(high)) set
+            else findPalindromes(s, low - 1, high + 1, set + s.substring(low, high + 1))
+
+        def findAllPalindromesUsingCenter(s: String): Set[String] =
+            (0 until s.length).toSet[Int] flatMap { i =>
+                findPalindromes(s, i, i, Set[String]()) ++ findPalindromes(s, i, i + 1, Set[String]())
+            }
+
+        println(findAllPalindromesUsingCenter("abcdefedefe"))
+    }
+
+    @Test
+    def x() {
+
     }
 }
